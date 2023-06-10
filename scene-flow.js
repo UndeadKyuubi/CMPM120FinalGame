181ec9d09d7a807452f384df8b40e206a6241112
Brandon Jacobson <bjacobson891@gmail.com>
2023-06-09 23:54:00 -0700

    fixed scene swapping while moving

--- scene-flow.js
+++ scene-flow.js
@@ -1,13 +1,22 @@
-class sceneFlow extends Phaser.Scene {
+const scenes = {
+    mainTitle: 'mainTitle',
+    credit: 'credit',
+    gameplay: 'gameplay',
+    menu: 'menu'
+};
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: []
+};
+
+const game = new Phaser.Game(config);
+
+//Main Menu scene
+class MainMenu extends Phaser.Scene {
     constructor() {
-        super('sceneflow');
-    }
-
-    preload() {
-
-    }
-
-    create() {
-        
+        super(scenes.mainTitle)
     }
 }
