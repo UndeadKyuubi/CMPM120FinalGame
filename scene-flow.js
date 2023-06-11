ee38e3f7e9e10526c086b19fd0579666054f5489
Jimmy Nguyen <nguyenjimmy627@gmail.com>
2023-06-10 17:47:47 -0700

    Added scenes and buttons to switch to them

--- scene-flow.js
+++ scene-flow.js
@@ -1,59 +1,113 @@
-const scenes = {
-    mainTitle: 'mainTitle',
-    credit: 'credit',
-    gameplay: 'gameplay',
-    menu: 'menu'
-};
-
-const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: [MainTitle, Credit, Gameplay, Menu]
-};
-
-const game = new Phaser.Game(config);
-
 //Main Menu scene
 class MainTitle extends Phaser.Scene {
     constructor() {
-        super(scenes.mainTitle)
+        super('mainTitle');
     }
 
     create() {
-        //this.add.image(0, 0, 'background')
+        let title = this.add.text((config.width / 2), (config.height / 2) - 300, "Main Menu");
+        title.setAlign("center");
+        title.setFontSize(120);
+        title.setOrigin(0.5, 0.5);
 
-        startButton.on('pointerup', () => {
-            this.scene.start(scenes.gameplay);
-        });
+        let playButton = this.add.text((config.width / 2), (config.height / 2) - 90, "Play");
+        playButton.setAlign("center");
+        playButton.setFontSize(70);
+        playButton.setOrigin(0.5, 0.5);
 
-        creditButton.on('pointerup', () => {
-            this.scene.start(scenes.credit);
-        });
+        playButton.setInteractive();
+        playButton.on(Phaser.Input.Events.POINTER_DOWN, () => {
+            this.scene.start('gameplay');
+        })
+
+        let creditButton = this.add.text((config.width / 2), (config.height / 2) - 0, "Credit");
+        creditButton.setAlign("center");
+        creditButton.setFontSize(70);
+        creditButton.setOrigin(0.5, 0.5);
+
+        creditButton.setInteractive();
+        creditButton.on(Phaser.Input.Events.POINTER_DOWN, () => {
+            this.scene.start('credit');
+        })
     }
 }
 
 class Credit extends Phaser.Scene {
     constructor() {
-        super(scenes.credit)
+        super('credit')
     }
 
     create() {
-
-        backButton.on('pointerup', () => {
-            this.scene.start(scenes.mainTitle);
-        });
+        this.add.text((config.width / 2), (config.height / 2)- 300, "This is the credit page")
+        .setAlign("center")
+        .setFontSize(70)
+        .setOrigin(0.5, 0.5);
     }
 }
 
 class Gameplay extends Phaser.Scene {
     constructor() {
-        super()
+        super('gameplay')
+    }
+
+    create() {
+        this.add.text((config.width / 2), (config.height / 2)- 300, "This is the gameplay page")
+        .setAlign("center")
+        .setFontSize(70)
+        .setOrigin(0.5, 0.5);
+
+        let menuButton = this.add.text(0, 0, "Menu");
+        menuButton.setOrigin(0, 0);
+        menuButton.setFontSize(50);
+        menuButton.setInteractive();
+
+        menuButton.setInteractive();
+        menuButton.on(Phaser.Input.Events.POINTER_DOWN, () => {
+            this.scene.start('menu');
+        })
     }
 }
 
 class Menu extends Phaser.Scene {
     constructor() {
-        super()
+        super('menu')
+    }
+
+    create() {
+        this.add.text((config.width / 2), (config.height / 2) - 300, "Menu")
+        .setAlign("center")
+        .setFontSize(70)
+        .setOrigin(0.5, 0.5);
+
+        let backButton = this.add.text((config.width / 2), (config.height / 2) - 120, "Back")
+        backButton.setAlign("center");
+        backButton.setFontSize(30);
+        backButton.setOrigin(0.5, 0.5);
+
+        backButton.setInteractive();
+        backButton.on(Phaser.Input.Events.POINTER_DOWN, () => {
+            this.scene.start('gameplay');
+        })
+
+        let quitButton = this.add.text((config.width / 2), (config.height / 2) - 90, "Quit game")
+        quitButton.setAlign("center");
+        quitButton.setFontSize(30);
+        quitButton.setOrigin(0.5, 0.5);
+
+        quitButton.setInteractive();
+        quitButton.on(Phaser.Input.Events.POINTER_DOWN, () => {
+            this.scene.start('mainTitle');
+        })
     }
 }
+
+
+const config = {
+    type: Phaser.AUTO,
+    width: 1920,
+    height: 1080,
+    //scene: [Gameplay]
+    scene: [MainTitle, Credit, Gameplay, Menu]
+};
+
+const game = new Phaser.Game(config);
