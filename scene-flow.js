ccff14a936636b2fb326f2c57f3fc206a22bda95
Jimmy Nguyen <nguyenjimmy627@gmail.com>
2023-06-10 18:50:29 -0700

    Added fancy transition

--- scene-flow.js
+++ scene-flow.js
@@ -1,3 +1,6 @@
+let gameplayScene = 0;
+let blockMoved = false;
+
 //Main Menu scene
 class MainTitle extends Phaser.Scene {
     constructor() {
@@ -17,7 +20,10 @@ class MainTitle extends Phaser.Scene {
 
         playButton.setInteractive();
         playButton.on(Phaser.Input.Events.POINTER_DOWN, () => {
-            this.scene.start('gameplay');
+            this.cameras.main.fadeOut(600, 0, 0, 0);
+            this.time.delayedCall(601, () => {
+                this.scene.start('gameplay');
+            })
         })
 
         let creditButton = this.add.text((config.width / 2), (config.height / 2) - 0, "Credit");
@@ -27,11 +33,14 @@ class MainTitle extends Phaser.Scene {
 
         creditButton.setInteractive();
         creditButton.on(Phaser.Input.Events.POINTER_DOWN, () => {
-            this.scene.start('credit');
+            this.cameras.main.fadeOut(600, 0, 0, 0);
+            this.time.delayedCall(601, () => {
+                this.scene.start('credit');
+            })
         })
     }
 }
-
+//Credit scene
 class Credit extends Phaser.Scene {
     constructor() {
         super('credit')
@@ -42,9 +51,21 @@ class Credit extends Phaser.Scene {
         .setAlign("center")
         .setFontSize(70)
         .setOrigin(0.5, 0.5);
+
+        let retrunToMainMenu = this.add.text((config.width / 2), (config.height / 2) + 90, "Return to Main Menu")
+        retrunToMainMenu.setAlign("center");
+        retrunToMainMenu.setFontSize(30);
+        retrunToMainMenu.setOrigin(0.5, 0.5);   
+
+        retrunToMainMenu.setInteractive();
+        retrunToMainMenu.on(Phaser.Input.Events.POINTER_DOWN, () => {
+            gameplayScene = 0;
+            this.scene.start('mainTitle');
+        })
     }
 }
 
+//Gameplay 1 & 2 scenes
 class Gameplay extends Phaser.Scene {
     constructor() {
         super('gameplay')
@@ -63,11 +84,129 @@ class Gameplay extends Phaser.Scene {
 
         menuButton.setInteractive();
         menuButton.on(Phaser.Input.Events.POINTER_DOWN, () => {
+            gameplayScene = 1;
             this.scene.start('menu');
+        })
+
+        let NextButton = this.add.text((config.width / 2), (config.height / 2) - 120, "Next Scene")
+        NextButton.setAlign("center");
+        NextButton.setFontSize(70);
+        NextButton.setOrigin(0.5, 0.5);
+
+        NextButton.setInteractive();
+        NextButton.on(Phaser.Input.Events.POINTER_DOWN, () => {
+            gameplayScene = 2;
+            this.cameras.main.fadeOut(600, 0, 0, 0);
+            this.time.delayedCall(601, () => {
+                this.scene.start('gameplay2');
+            })
+        })
+
+        let block = this.add.text((config.width / 2), (config.height / 2) - 30, "Block (Not pushed)")
+
+        if (blockMoved == true) {
+            block.setText("Block (Pushed)")
+        }
+        else {
+            block.setText("Block (Not pushed)")
+        }
+
+        block.setAlign("center");
+        block.setFontSize(50);
+        block.setOrigin(0.5, 0,5);
+
+        block.setInteractive();
+        block.on(Phaser.Input.Events.POINTER_DOWN, () => {
+            if (blockMoved == false) {
+                blockMoved = true;
+                block.setText("Block (Pushed)")
+            }
+            else {
+                blockMoved = false;
+                block.setText("Block (Not pushed)")
+            }
         })
     }
 }
 
+class Gameplay2 extends Phaser.Scene {
+    constructor() {
+        super('gameplay2')
+    }
+
+    create() {
+        this.add.text((config.width / 2), (config.height / 2)- 300, "This is the gameplay page 2")
+        .setAlign("center")
+        .setFontSize(70)
+        .setOrigin(0.5, 0.5);
+
+        let menuButton = this.add.text(0, 0, "Menu");
+        menuButton.setOrigin(0, 0);
+        menuButton.setFontSize(50);
+        menuButton.setInteractive();
+
+        menuButton.setInteractive();
+        menuButton.on(Phaser.Input.Events.POINTER_DOWN, () => {
+            gameplayScene = 2;
+            this.scene.start('menu');
+        })
+        let prevScene = this.add.text((config.width / 2), (config.height / 2) - 120, "Previous Scene")
+        prevScene.setAlign("center");
+        prevScene.setFontSize(70);
+        prevScene.setOrigin(0.5, 0.5);   
+
+        prevScene.setInteractive();
+        prevScene.on(Phaser.Input.Events.POINTER_DOWN, () => {
+            gameplayScene = 1;
+            this.cameras.main.fadeOut(600, 0, 0, 0);
+            this.time.delayedCall(601, () => {
+                this.scene.start('gameplay');
+            })
+        })
+
+
+
+        let block = this.add.text((config.width / 2), (config.height / 2) - 30, "Block (Not pushed)")
+        if (blockMoved == true) {
+            block.setText("Block (Pushed)")
+        }
+        else {
+            block.setText("Block (Not pushed)")
+        }
+
+        block.setAlign("center");
+        block.setFontSize(50);
+        block.setOrigin(0.5, 0,5);
+
+        block.setInteractive();
+        block.on(Phaser.Input.Events.POINTER_DOWN, () => {
+            if (blockMoved == false) {
+                blockMoved = true;
+                block.setText("Block (Pushed)")
+            }
+            else {
+                blockMoved = false;
+                block.setText("Block (Not pushed)")
+            }
+        })
+
+        let retrunToMainMenu = this.add.text((config.width / 2), (config.height / 2) + 90, "Return to Main Menu")
+        retrunToMainMenu.setAlign("center");
+        retrunToMainMenu.setFontSize(70);
+        retrunToMainMenu.setOrigin(0.5, 0.5);   
+
+        retrunToMainMenu.setInteractive();
+        retrunToMainMenu.on(Phaser.Input.Events.POINTER_DOWN, () => {
+            gameplayScene = 0;
+            this.cameras.main.fadeOut(600, 0, 0, 0);
+            this.time.delayedCall(601, () => {
+                this.scene.start('mainTitle');
+            })
+        })
+    }
+}
+
+//Menu scene
 class Menu extends Phaser.Scene {
     constructor() {
         super('menu')
@@ -86,7 +225,13 @@ class Menu extends Phaser.Scene {
 
         backButton.setInteractive();
         backButton.on(Phaser.Input.Events.POINTER_DOWN, () => {
-            this.scene.start('gameplay');
+            if (gameplayScene == 1) {
+                this.scene.start('gameplay');
+            }
+            if (gameplayScene == 2) {
+                this.scene.start('gameplay2');
+            }
+
         })
 
         let quitButton = this.add.text((config.width / 2), (config.height / 2) - 90, "Quit game")
@@ -107,7 +252,7 @@ const config = {
     width: 1920,
     height: 1080,
     //scene: [Gameplay]
-    scene: [MainTitle, Credit, Gameplay, Menu]
+    scene: [MainTitle, Credit, Gameplay, Gameplay2, Menu]
 };
 
 const game = new Phaser.Game(config);
