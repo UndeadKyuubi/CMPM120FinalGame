5f3fab4595024e6a6637a18dec566708fe71e560
Brandon Jacobson <bjacobson891@gmail.com>
2023-06-11 20:19:50 -0700

    Added sprite animations to character

--- game.js
+++ game.js
@@ -92,6 +92,10 @@ class YoreLevel1 extends Phaser.Scene{
         this.load.audio('music',"./assets/Radwimps_-_Date.mp3")
         this.load.audio('scrape',"./assets/80092__ayliffe__scrape-3.wav")
         this.load.image('yore', './assets/test sprite.png')
+        this.load.spritesheet('animYore', './assets/YoreSpriteSheetTest.png', {
+            frameWidth: 128, 
+            frameHeight: 128
+        });
     }
 
     create(){
@@ -106,7 +110,44 @@ class YoreLevel1 extends Phaser.Scene{
         let background= this.physics.add.image(this.sys.game.config.width / 2,this.sys.game.config.height / 2,'background');
         this.physics.world.setBounds(0,0,3000,3000);
 
-        this.player=this.physics.add.sprite(this.sys.game.config.width / 2,this.sys.game.config.height / 2,"yore").setScale(.1);
+        //Yore Sprites
+        this.anims.create({
+            key: 'idleYore',
+            frames: this.anims.generateFrameNumbers('animYore', {frames: [0, 1, 2, 3, 4]}),
+            frameRate: 5,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'walkLYore',
+            frames: this.anims.generateFrameNumbers('animYore', {frames: [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]}),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'walkRYore',
+            frames: this.anims.generateFrameNumbers('animYore', {frames: [20, 21, 22, 23, 24, 25, 26, 27, 28, 29]}),
+            frameRate: 10,
+            repeat: -1
+        })
+
+        this.anims.create({
+            key: 'walkUYore',
+            frames: this.anims.generateFrameNumbers('animYore', {frames: [30, 31, 32, 33, 34, 35]}),
+            frameRate: 6,
+            repeat: -1
+        })
+
+        this.anims.create({
+            key: 'walkDYore',
+            frames: this.anims.generateFrameNumbers('animYore', {frames: [40, 41, 42, 43, 44, 45]}),
+            frameRate: 6,
+            repeat: -1
+        })
+
+        this.player=this.physics.add.sprite(this.sys.game.config.width / 2,this.sys.game.config.height / 2,"animYore");
+        this.player.play('idleYore'); //play idle
         this.player.setCollideWorldBounds(true, 0, 0);
         
         this.target = new Phaser.Math.Vector2();
@@ -120,7 +161,7 @@ class YoreLevel1 extends Phaser.Scene{
             this.target = this.cameras.main.getWorldPoint(pointer.x, pointer.y);
 
             //Move at 200 px/s:
-            this.physics.moveToObject(this.player, this.target, 200);
+            this.playerAngle = this.physics.moveToObject(this.player, this.target, 200);
         });
 
         //Block push
@@ -133,6 +174,7 @@ class YoreLevel1 extends Phaser.Scene{
         this.physics.add.collider(this.player, this.testBlock);
 
         this.scene.launch('hud');
+
     }
 
     update() {
@@ -141,6 +183,8 @@ class YoreLevel1 extends Phaser.Scene{
 
         const distance = Phaser.Math.Distance.BetweenPoints(this.player, this.target);
 
+        let angle = this.playerAngle * (180/Math.PI);
+
         if (this.player.body.speed > 0)
         {
             isMoving = true;
@@ -148,9 +192,25 @@ class YoreLevel1 extends Phaser.Scene{
             {
                 this.player.body.reset(this.target.x, this.target.y);
             }
+
+            if (this.player.anims.isPlaying && this.player.anims.currentAnim.key != 'walkLYore' && ((angle < -135 && angle >= -180) || (angle > 135 && angle <= 180))) {
+                this.player.play('walkLYore');
+            }
+            else if (this.player.anims.isPlaying && this.player.anims.currentAnim.key != 'walkRYore' && ((angle > -45 && angle <= 0) || (angle < 45 && angle >= 0))){
+                this.player.play('walkRYore');
+            }
+            else if (this.player.anims.isPlaying && this.player.anims.currentAnim.key != 'walkUYore' && ((angle <= 135 && angle >= 90) || (angle >= 45 && angle <= 90))){
+                this.player.play('walkUYore');
+            }
+            else if (this.player.anims.isPlaying && this.player.anims.currentAnim.key != 'walkDYore' && ((angle >= -135 && angle <= -90) || (angle <= -45 && angle >= -90))){
+                this.player.play('walkDYore');
+            }
         }
         else{
             isMoving = false;
+            if (this.player.anims.isPlaying && this.player.anims.currentAnim.key != 'idleYore') {
+                this.player.play('idleYore');
+            }
         }
 
         if (this.testBlock.body.speed > 0) {
@@ -321,6 +381,10 @@ class HUD extends Phaser.Scene {
     constructor(){
         super('hud');
     }
+
+    // when we move to a new set of levels we can shutdown the old levels, start the next yore level
+    // and when we first swap to new future level, it will launch the next neo level
+    // Probably need to set swapFuture and swapPast back to 0 upon moving to new set of levels
 
     create(){
         this.swapButton = this.add.rectangle(150, 1000, 200, 75, 0xababab, 1).setInteractive();
